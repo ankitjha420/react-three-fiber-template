0921e28d4f9055a4402cfa1f006f6d2f8e9ceefb
ankitjha420 <ankitjha11thegreat@gmail.com>
2023-08-21 22:22:53 +0530

    first

--- src/index.js
+++ src/index.js
@@ -1,17 +1,46 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client'
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls, Stars } from "@react-three/drei";
+import { Physics, usePlane, useBox } from "@react-three/cannon";
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+function Box() {
+	const [ref, api] = useBox(() => ({ mass: 1, position: [0, 2, 0] }));
+	return (
+		<mesh
+			onClick={() => {
+				api.velocity.set(0, 2, 0);
+			}}
+			ref={ref}
+			position={[0, 2, 0]}
+		>
+			<boxGeometry attach="geometry" />
+			<meshLambertMaterial attach="material" color="hotpink" />
+		</mesh>
+	);
+}
+
+function Plane() {
+	const [ref] = usePlane(() => ({
+		rotation: [-Math.PI / 2, 0, 0],
+	}));
+	return (
+		<mesh ref={ref} rotation={[-Math.PI / 2, 0, 0]}>
+			<planeGeometry attach="geometry" args={[100, 100]} />
+			<meshLambertMaterial attach="material" color="lightblue" />
+		</mesh>
+	);
+}
+
+createRoot(document.getElementById('root')).render(
+	<Canvas>
+		<OrbitControls />
+		<Stars />
+		<ambientLight intensity={0.5} />
+		<spotLight position={[10, 15, 10]} angle={0.3} />
+		<Physics>
+			<Box />
+			<Plane />
+		</Physics>
+	</Canvas>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
